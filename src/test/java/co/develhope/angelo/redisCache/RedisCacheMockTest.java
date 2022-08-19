60bbed7f29bc5308c07b679d249582269eeb1d55
Angelo Zammataro <juba.kem@gmail.com>
2022-08-19 20:29:41 +0200

    Third Commit: Redis Cache Example - [parte 1/4] + [parte 2/4] + [parte 3/4] + [parte 4/4] App run OK; CRUD Operations: OK; Add tests

--- src/test/java/co/develhope/angelo/redisCache/RedisCacheMockTest.java
+++ src/test/java/co/develhope/angelo/redisCache/RedisCacheMockTest.java
@@ -1,7 +1,9 @@
 package co.develhope.angelo.redisCache;
 
+import co.develhope.angelo.redisCache.entities.jpa.UserJPA;
 import co.develhope.angelo.redisCache.entities.redis.UserRedis;
 import co.develhope.angelo.redisCache.repositories.redis.UserRepositoryRedis;
+import co.develhope.angelo.redisCache.services.UserService;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
@@ -16,15 +18,25 @@ public class RedisCacheMockTest {
     @Autowired
     private UserRepositoryRedis userRepositoryRedis;
 
+    @Autowired
+    private UserService userService;
+
     @Test
     public void shouldWriteOneRedisCache(){
         UserRedis userRedis = new UserRedis();
-        userRedis.setDomicileCity("Roma");
-        userRedis.setEmail("pippo@pippo.com");
+        userRedis.setDomicileCity("myCity");
+        userRedis.setId(1L);
+        userRedis.setEmail("example@example.com");
         userRedis.setFirstName("myName");
         userRedis.setLastName("mySurname");
 
         UserRedis userSavedInRedisCache = userRepositoryRedis.save(userRedis);
         Assertions.assertNotNull(userSavedInRedisCache);
+    }
+
+    @Test
+    public void shouldReadOneRedisCache(){
+        UserJPA userReadInRedisCache = userService.readOne(1L);
+        Assertions.assertNotNull(userReadInRedisCache);
     }
 }
